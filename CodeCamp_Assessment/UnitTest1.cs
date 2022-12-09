bcd24bd7d81d8cfab7e768f6ddb608a84d7b31a1
BlueBell17 <niccoe_general@outlook.com>
2022-12-09 12:57:06 +1000

    Initial commit

---
+++ CodeCamp_Assessment/UnitTest1.cs
@@ -0,0 +1,52 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace CodeCamp_Assessment
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        #pragma warning disable CS8618
+        private IWebDriver _driver;
+        #pragma warning restore CS8618
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _driver = new ChromeDriver();
+            _driver.Url = "website";
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+
+
+            // Act
+
+
+            // Run
+
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            // Arrange
+
+
+            // Act
+
+
+            // Run
+
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            _driver.Quit();
+        }
+    }
+}
